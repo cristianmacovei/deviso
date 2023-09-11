897cba47ddeef8e77a671a544212f240a0db6383
Cristian Macovei <118800887+cristianmacovei@users.noreply.github.com>
2023-09-11 13:27:59 +0300

    dd

--- src/Content.js
+++ src/Content.js
@@ -40,7 +40,7 @@ export default function Content() {
   // Add event listener when component mounts
   useEffect(() => {
     if (isLeftOpen || isRightOpen) {
-      //console.log(process.env.REACT_APP_GITHUB_API_TOKEN);
+      console.log(process.env.REACT_APP_GITHUB_API_TOKEN);
       disableBodyScroll();
     } else {
       enableBodyScroll();
